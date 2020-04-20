5af1a952645de4116ffd286c8ec7b0f02576cf2e
Tashby <tashby2714@gmail.com>
2020-04-19 21:06:58 -0400

    cards

--- components/Cards/index.js
+++ components/Cards/index.js
@@ -18,3 +18,61 @@
 // </div>
 //
 // Use your function to create a card for each of the articles and add the card to the DOM.
+
+
+
+function createCard(article){
+    //card
+    const myCard = document.createElement('div');
+    myCard.classList.add('card');
+
+    //headline
+    const myHeadline = document.createElement('div');
+    myHeadline.classList.add('headline');
+    myHeadline.textContent = article.headline;
+    myCard.appendChild(myHeadline);
+
+    //author
+    const myAuthor = document.createElement('div');
+    myAuthor.classList.add('author');
+
+    //img container
+    const imgContainer = document.createElement('div');
+    imgContainer.classList.add('img-container');
+
+    //authors img
+    const authorsImg = document.createElement('img');
+    authorsImg.src = article.authorPhoto;
+    imgContainer.appendChild(authorsImg);
+    myAuthor.appendChild(imgContainer);
+
+    //authors name
+    const authorsName = document.createElement('span');
+    authorsName.textContent = article.authorName;
+    myAuthor.appendChild(authorsName);
+
+    //append
+    myCard.appendChild(myAuthor);
+
+    //return  
+    return myCard;
+
+    
+}
+
+axios.get(`https://lambda-times-backend.herokuapp.com/articles`)
+    .then((response) => {
+        
+        for (let article in response.data.articles){
+            
+            response.data.articles[article].map((item)=>{
+                console.log(item);
+                const newArticle = document.querySelector('.cards-container');
+                newArticle.appendChild(createCard(item));
+            })
+        }
+        
+    })
+    .catch((error) => { 
+        console.log(error);
+    })
